6248f0f074a64c305d718bea99a837d38cc09fbc
Anton-Ca <anton-ca@outlook.com>
2023-02-19 12:52:34 +0100

    Valentines Jess

--- Django/src/jess/views.py
+++ Django/src/jess/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def jess_view(request, *args, **kwargs):
     """ Jess page. """
     return render(request, "jess.html", {})
+
+
+def jess_heart(request, *args, **kwargs):
+    """ Jess valentines solution/reward page. """
+    return render(request, "jess_heart.html", {})
+
+
+def jess_quiz(request, *args, **kwargs):
+    """ Jess valentines quiz page. """
+    return render(request, "jess_quiz.html", {})
--- Django/src/trydjango/urls.py
+++ Django/src/trydjango/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 # Own
 from homepage.views import home_view
 from contact.views import contact_view
-from jess.views import jess_view
+from jess.views import jess_view, jess_heart, jess_quiz
 from comingSoon.views import coming_soon_view
 from todo.views import todo_view
 
@@ -28,6 +28,9 @@ urlpatterns = [
     path("home/", home_view, name='home'),
     path("contact/", contact_view, name='contact'),
     path("jess/", jess_view, name='jess'),
+    path("a<3j/", jess_heart, name='anton<3jess'),
+    path("A<3J/", jess_heart, name='anton<3jess'),
+    path("jess_quiz/", jess_quiz, name='quiz'),
     path("coming-soon/", coming_soon_view, name='coming-soon'),
     path("todo/", todo_view, name='todo'),
     path("admin/", admin.site.urls),
